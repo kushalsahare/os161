63acb495b3c7e0737d124d865fc6e408cb97483c
Kushal Sahare <ksh.sah@gmail.com>
2018-04-10 18:52:18 -0400

    first commit for condition variable

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -302,7 +302,12 @@ cv_create(const char *name)
 	}
 
 	// add stuff here as needed
-
+	cv->cv_wchan = wchan_create(lock->lk_name);
+	if(lock->lk_wchan == NULL){
+		kfree(lock->lk_name);
+		kfree(lock);
+		return NULL;
+	}
 	return cv;
 }
 
